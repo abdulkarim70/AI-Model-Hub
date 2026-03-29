e45be9218a42d3d50c9815879ce7d66343b3a1cd
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-03-29 20:46:36 +0600

    cart counted

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Cart = ({carts}) => {
-    console.log(carts);
+  const totalPrice=carts.reduce((sum, cart)=> sum+cart.price, 0)
     return (
         <div className='p-10'>
             <h2 className='text-2xl font-bold'>Your Carts</h2>
-       {carts.map(cart=> <div className='flex items-center justify-between border rounded-lg px-2' key={cart.id}>
+<div className='space-y-5 mt-5'>
+           {carts.map(cart=> <div className='flex items-center justify-between border rounded-lg px-2 p-4' key={cart.id}>
 <div className='flex items-center gap-2'>
     <div className='py-5'>
     <img className='h-20 w-20 object-contain' src={cart.image} alt="" />
@@ -16,6 +17,11 @@ const Cart = ({carts}) => {
 </div>
 <div className='text-3xl font-bold'>$ {cart.price}/Per Month</div>
        </div> )} 
+</div>
+<div className=' flex justify-between bg-black text-white p-5 mt-5 rounded-lg text-3xl font-bold'>
+<div>Total</div>
+<div>$ {totalPrice}</div>
+</div>
         </div>
     );
 };
